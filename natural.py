1f161ac2bb33ba77f203c37ab3de273530d4bb9e
englishta <1999ju373@gmail.com>
2021-05-14 12:14:36 +0900

    a

--- natural.py
+++ natural.py
@@ -25,21 +25,21 @@ corpus = [word_to_id[w] for w in words]
 corpus = np.array(corpus)
 corpus
 # %%
-def preprocess(text):
-    text = text.lower()
-    text = text.replace('.', ' .')
-    words  = text.split(' ')
+# def preprocess(text):
+#     text = text.lower()
+#     text = text.replace('.', ' .')
+#     words  = text.split(' ')
 
-    word_to_id = {}
-    id_to_word = {}
-    for word in words:
-        if word not in word_to_id:
-            new_id = len(word_to_id)
-            word_to_id[word] = new_id
-            id_to_word[new_id] = word
-    corpus = np.array([word_to_id[e] for e in words])
+#     word_to_id = {}
+#     id_to_word = {}
+#     for word in words:
+#         if word not in word_to_id:
+#             new_id = len(word_to_id)
+#             word_to_id[word] = new_id
+#             id_to_word[new_id] = word
+#     corpus = np.array([word_to_id[e] for e in words])
 
-    return corpus, word_to_id, id_to_word
+#     return corpus, word_to_id, id_to_word
 
 # %%
 print(preprocess("yorosiku onegai simasu"))
@@ -74,6 +74,7 @@ def create_co_matrix(corpus, vocab_size, window_size=1):
 # %%
 import numpy as np
 from util import preprocess
+
 text = "You say goodbye and I say hellow."
 corpus, word_to_id, id_to_word = preprocess(text)
 print(corpus)
