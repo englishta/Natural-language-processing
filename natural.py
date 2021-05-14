4d2f9204e564c4076e1a4927356a5782a3777ca6
englishta <1999ju373@gmail.com>
2021-05-14 09:40:06 +0900

    a

--- natural.py
+++ natural.py
@@ -53,3 +53,19 @@ from util import preprocess
 text = "You say goodbye and I say hellow."
 print(preprocess(text))
 # %%
+# 共起行列をつくる関数 p72
+def create_co_matrix(corpus, vocab_size, window_size=1):
+    corpus_size = len(corpus)
+    co_matrix = np.size((vocab_size, vocab_size), dtype = np.int32)
+
+    for idx, word_id in enumerate(corpus):
+        for i in range(1, window_size + 1):
+            left_idx = idx-i
+            right_idx = idx+i
+            if left_idx >= 0:
+                left_word_id = corpus[left_idx]
+                co_matrix[word_id, left_word_id]+=1
+            if right_idx < corpus_size:
+                right_word_id = corpus[right_idx]
+                co_matrix[word_id, right_word_id]+=1
+    return co_matrix
