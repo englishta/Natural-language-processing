d583b75ab0431b6334ce859c06b42e7d3511365f
englishta <1999ju373@gmail.com>
2021-05-14 13:35:03 +0900

    k

--- natural.py
+++ natural.py
@@ -39,11 +39,12 @@ print(preprocess(text))
 
 # %%
 import numpy as np
-from util import create_co_matrix, preprocess
+from util import create_co_matrix, preprocess, cos_similarity
 
 text = "You say goodbye and I say hellow."
 corpus, word_to_id, id_to_word = preprocess(text)
-matrix = create_co_matrix(corpus, len(word_to_id))
-print(matrix)
-
+C = create_co_matrix(corpus, len(word_to_id))
+c0 = C[word_to_id['you']]
+c2 = C[word_to_id['i']]
+print(cos_similarity(c0, c2))
 # %%
