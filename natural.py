218b8198f86a7fbc4ea32939543b65c728de4fd3
englishta <1999ju373@gmail.com>
2021-05-29 14:20:26 +0900

    a

--- natural.py
+++ natural.py
@@ -27,3 +27,24 @@ print('-'*50)
 print('PPMI')
 print(W)
 # %%
+# SVDによる次元削減p84
+import numpy as np
+from util import create_co_matrix, preprocess, cos_similarity, ppmi
+text = "You say goodbye and I say hello."
+corpus, word_to_id, id_to_word = preprocess(text)
+W = ppmi(C, True)
+U, S, V = np.linalg.svd(W)
+#%%
+print(C[0])
+print(W[0])
+print(U[0])
+print(U[0, :2])
+# %%
+# p85
+import matplotlib.pyplot as plt
+for word, word_id in word_to_id.items():
+    plt.annotate(word, (U[word_id, 0], U[word_id, 1]))
+plt.scatter(U[:,0], U[:,1], alpha=0.5)
+plt.show()
+
+# %%
