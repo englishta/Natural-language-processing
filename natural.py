85cef56e980a1db3c8e39e22890104ec23532f67
englishta <1999ju373@gmail.com>
2021-05-29 13:28:46 +0900

    a

--- natural.py
+++ natural.py
@@ -13,3 +13,17 @@ print(cos_similarity(c0, c2))
 from util import most_similar
 most_similar('hellow', word_to_id, id_to_word, C)
 # %%
+# 共起行列をppmi行列に変換する
+import numpy as np
+from util import create_co_matrix, preprocess, cos_similarity, ppmi
+text = "You say goodbye and I say hellow."
+corpus, word_to_id, id_to_word = preprocess(text)
+W = ppmi(C, True)
+
+np.set_printoptions(precision=3)
+print('covariance matrix')
+print(C)
+print('-'*50)
+print('PPMI')
+print(W)
+# %%
